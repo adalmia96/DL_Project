eaf48ebfbcce243aca12100fff40b452615a30a4
Taryn <tarynw6@gmail.com>
2020-04-14 14:58:21 -1000

    adding relevant Schultz 2019 files

--- main.py
+++ main.py
@@ -1,47 +1,23 @@
-import argparse
-
-def get_args():
-    """Get arguments from command line."""
-    # Data files arguments
-    args_parser.add_argument(
-        '--train-file',
-        help='Name of training file. If using Docker/GCS script, must use exact name stored under GCS bucket.',
-        nargs='+',
-        required=True)
-    args_parser.add_argument(
-        '--test-file',
-        help='Name of dev file. If using Docker/GCS script, must use exact name stored under GCS bucket.',
-        nargs='+',
-        required=True)
-    # Experiment arguments
-    args_parser.add_argument(
-        '--batch-size',
-        help='Batch size for each training and evaluation step.',
-        type=int,
-        default=100)
-    args_parser.add_argument(
-        '--num-epochs',
-        help='Maximum number of training data epochs on which to train.',
-        default=50,
-        type=int,
-        )
-    return args_parser.parse_args()
-
-def main():
-    """Setup"""
-    args = get_args()
-    print args
-    if args.model == 'wgan2d':
-        import models.wgantwod
-        models.wgan2d.train()
-        models.wgan2d.test()
-    else if args.model == 'fake':
-        import models.fake
-        models.fake.train()
-        models.fake.test()
-    else:
-        print("Invalid model!")
-    return
-
+# main
 if __name__ == "__main__":
-    main()
+    while True:
+        model = input("Select model to run (dcgan1, dcgan2, wgan1d, wgan2d): ")
+        try:
+            if model == "dcgan1":
+                import models.dcganone
+                models.dcganone.train(0)
+            elif model == "dcgan2":
+                import models.dcgantwo
+                models.dcgantwo.test()
+            elif model == "wgan1d":
+                import models.wganoned
+                models.wganoned.train()
+            elif model == "wgan2d":
+                import models.wgantwod
+                models.wgantwod.test()
+            else:
+                raise ValueError
+        except ValueError:
+            print("")
+        else:
+            break
